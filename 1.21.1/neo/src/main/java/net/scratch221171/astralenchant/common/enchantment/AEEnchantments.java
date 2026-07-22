74dd3a324fec382e644a6d8c7fabc5da45d1f22c
scratch221171 <scratch221171@gmail.com>
2026-07-22 15:55:45 +0900

    chore: spotless in "common"

--- 1.21.1/neo/src/main/java/net/scratch221171/astralenchant/common/enchantment/AEEnchantments.java
+++ 1.21.1/neo/src/main/java/net/scratch221171/astralenchant/common/enchantment/AEEnchantments.java
@@ -12,45 +12,20 @@ public class AEEnchantments {
 
     public static final Set<ResourceKey<Enchantment>> LIST = new LinkedHashSet<>();
 
-    public static final ResourceKey<Enchantment> NULLIFICATION = create(ID.NULLIFICATION);
-
-    public static final ResourceKey<Enchantment> LAST_STAND = create(ID.LAST_STAND);
-
-    //    public static final ResourceKey<Enchantment> ITEM_PROTECTION = create(ID.ITEM_PROTECTION);
-
-    public static final ResourceKey<Enchantment> ESSENCE_OF_ENCHANTMENT = create(ID.ESSENCE_OF_ENCHANTMENT);
-
-    public static final ResourceKey<Enchantment> RESILIENCE = create(ID.RESILIENCE);
-
-    public static final ResourceKey<Enchantment> FEATHER_TOUCH = create(ID.FEATHER_TOUCH);
-
-    //    public static final ResourceKey<Enchantment> ADVENTURERS_LORE = create(ID.ADVENTURERS_LORE);
-
-    public static final ResourceKey<Enchantment> AFFINITY = create(ID.AFFINITY);
-
-    public static final ResourceKey<Enchantment> ENDLESS_APPETITE = create(ID.ENDLESS_APPETITE);
-
-    //    public static final ResourceKey<Enchantment> MOMENTUM = create(ID.MOMENTUM);
-
-    //    public static final ResourceKey<Enchantment> INSTANT_TELEPORT = create(ID.INSTANT_TELEPORT);
-
-    public static final ResourceKey<Enchantment> OVERLOAD = create(ID.OVERLOAD);
-
-    public static final ResourceKey<Enchantment> EXPANSE = create(ID.EXPANSE);
-
-    public static final ResourceKey<Enchantment> REACTIVE_ARMOR = create(ID.REACTIVE_ARMOR);
-
-    //    public static final ResourceKey<Enchantment> MYSTIC_REMNANTS = create(ID.MYSTIC_REMNANTS);
-
-    public static final ResourceKey<Enchantment> CURSE_OF_IGNORANCE = create(ID.CURSE_OF_IGNORANCE);
-
-    public static final ResourceKey<Enchantment> CURSE_OF_ENCHANTMENT = create(ID.CURSE_OF_ENCHANTMENT);
-
-    public static final ResourceKey<Enchantment> DISTORTION = create(ID.DISTORTION);
-
-    //    public static final ResourceKey<Enchantment> OVER_MENDING = create(ID.OVER_MENDING);
-
-    public static final ResourceKey<Enchantment> ALMIGHTY = create(ID.ALMIGHTY);
+    public static final ResourceKey<Enchantment> NULLIFICATION = create(ID.NULLIFICATION),
+            LAST_STAND = create(ID.LAST_STAND),
+            ESSENCE_OF_ENCHANTMENT = create(ID.ESSENCE_OF_ENCHANTMENT),
+            RESILIENCE = create(ID.RESILIENCE),
+            FEATHER_TOUCH = create(ID.FEATHER_TOUCH),
+            AFFINITY = create(ID.AFFINITY),
+            ENDLESS_APPETITE = create(ID.ENDLESS_APPETITE),
+            OVERLOAD = create(ID.OVERLOAD),
+            EXPANSE = create(ID.EXPANSE),
+            REACTIVE_ARMOR = create(ID.REACTIVE_ARMOR),
+            CURSE_OF_IGNORANCE = create(ID.CURSE_OF_IGNORANCE),
+            CURSE_OF_ENCHANTMENT = create(ID.CURSE_OF_ENCHANTMENT),
+            DISTORTION = create(ID.DISTORTION),
+            ALMIGHTY = create(ID.ALMIGHTY);
 
     private static ResourceKey<Enchantment> create(String name) {
         var key = ResourceKey.create(Registries.ENCHANTMENT, ModUtils.loc(name));
--- common/src/config/java/net/scratch221171/astralenchant/config/ServerConfig.java
+++ common/src/config/java/net/scratch221171/astralenchant/config/ServerConfig.java
@@ -1,44 +1,43 @@
 package net.scratch221171.astralenchant.config;
 
+import java.util.List;
 import net.scratch221171.astralenchant.mdk.config.ConfigEntries;
 import net.scratch221171.astralenchant.mdk.config.ConfigEntry;
 import net.scratch221171.astralenchant.mdk.config.ConfigEntryBuilder;
-
-import java.util.List;
 
 public class ServerConfig {
     private static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
 
     public static final ConfigEntries ENCHANTMENT_SETTINGS =
-            BUILDER.comment("Enchantment Settings")
-                    .category("enchantment_settings", EnchantmentSettings.ENTRIES);
+            BUILDER.comment("Enchantment Settings").category("enchantment_settings", EnchantmentSettings.ENTRIES);
 
     public static final class EnchantmentSettings {
         private static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
 
         public static final ConfigEntries NULLIFICATION =
-                BUILDER.comment("Nullification")
-                        .category(ConfigID.NULLIFICATION, Nullification.ENTRIES);
+                BUILDER.comment("Nullification").category(ConfigID.NULLIFICATION, Nullification.ENTRIES);
 
         public static final class Nullification {
-            private  static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
+            private static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
 
             // ResourceLocationを使えないのでしょうがない
-            public static final ConfigEntry.ListEntry<String> EXTRA_TAG = BUILDER.defineList(ConfigID.EXTRA_TAG,
-                    List.of("minecraft:bypasses_armor",
+            public static final ConfigEntry.ListEntry<String> EXTRA_TAG = BUILDER.defineList(
+                    ConfigID.EXTRA_TAG,
+                    List.of(
+                            "minecraft:bypasses_armor",
                             "minecraft:bypasses_cooldown",
                             "minecraft:bypasses_effects",
                             "minecraft:bypasses_enchantments",
                             "minecraft:bypasses_invulnerability",
-                            "minecraft:bypasses_shield"
-                            ), () -> "minecraft:bypasses_armor", e -> true);
+                            "minecraft:bypasses_shield"),
+                    () -> "minecraft:bypasses_armor",
+                    e -> true);
 
             public static final ConfigEntries ENTRIES = BUILDER.build();
         }
 
         public static final ConfigEntries LAST_STAND =
-                BUILDER.comment("Last Stand")
-                        .category(ConfigID.LAST_STAND, LastStand.ENTRIES);
+                BUILDER.comment("Last Stand").category(ConfigID.LAST_STAND, LastStand.ENTRIES);
 
         public static final class LastStand {
             private static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
@@ -61,9 +60,8 @@ public class ServerConfig {
             public static final ConfigEntries ENTRIES = BUILDER.build();
         }
 
-        public static final ConfigEntries ESSENCE_OF_ENCHANTMENT =
-                BUILDER.comment("Essence of Enchantment")
-                        .category(ConfigID.ESSENCE_OF_ENCHANTMENT, EssenceOfEnchantment.ENTRIES);
+        public static final ConfigEntries ESSENCE_OF_ENCHANTMENT = BUILDER.comment("Essence of Enchantment")
+                .category(ConfigID.ESSENCE_OF_ENCHANTMENT, EssenceOfEnchantment.ENTRIES);
 
         public static final class EssenceOfEnchantment {
             private static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
@@ -83,25 +81,25 @@ public class ServerConfig {
         }
 
         public static final ConfigEntries FEATHER_TOUCH =
-                BUILDER.comment("Feather touch")
-                        .category(ConfigID.FEATHER_TOUCH, FeatherTouch.ENTRIES);
+                BUILDER.comment("Feather touch").category(ConfigID.FEATHER_TOUCH, FeatherTouch.ENTRIES);
 
         public static final class FeatherTouch {
-            private  static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
+            private static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
 
-            public static final ConfigEntry.BooleanEntry SAVE_BLOCK_ENTITY = BUILDER.define(ConfigID.SAVE_BLOCK_ENTITY, true);
+            public static final ConfigEntry.BooleanEntry SAVE_BLOCK_ENTITY =
+                    BUILDER.define(ConfigID.SAVE_BLOCK_ENTITY, true);
 
-            public static final ConfigEntry.BooleanEntry SAVE_BLOCK_STATE = BUILDER.define(ConfigID.SAVE_BLOCK_STATE, true);
+            public static final ConfigEntry.BooleanEntry SAVE_BLOCK_STATE =
+                    BUILDER.define(ConfigID.SAVE_BLOCK_STATE, true);
 
             public static final ConfigEntries ENTRIES = BUILDER.build();
         }
 
         public static final ConfigEntries EXPANSE =
-                BUILDER.comment("Expanse")
-                        .category(ConfigID.EXPANSE, Expanse.ENTRIES);
+                BUILDER.comment("Expanse").category(ConfigID.EXPANSE, Expanse.ENTRIES);
 
         public static final class Expanse {
-            private  static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
+            private static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
 
             public static final ConfigEntry.BooleanEntry STACKABLE = BUILDER.define(ConfigID.STACKABLE, false);
 
@@ -109,23 +107,23 @@ public class ServerConfig {
         }
 
         public static final class ReactiveArmor {
-            private  static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
+            private static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
 
             // ResourceLocationを使えないのでしょうがない
-            public static final ConfigEntry.ListEntry<String> DISABLED_TAG = BUILDER.defineList(ConfigID.DISABLED_TAG,
-                    List.of("minecraft:bypasses_armor",
-                            "minecraft:bypasses_enchantments"
-                    ), () -> "minecraft:bypasses_armor", e -> true);
+            public static final ConfigEntry.ListEntry<String> DISABLED_TAG = BUILDER.defineList(
+                    ConfigID.DISABLED_TAG,
+                    List.of("minecraft:bypasses_armor", "minecraft:bypasses_enchantments"),
+                    () -> "minecraft:bypasses_armor",
+                    e -> true);
 
             public static final ConfigEntries ENTRIES = BUILDER.build();
         }
 
         public static final ConfigEntries CURSE_OF_IGNORANCE =
-                BUILDER.comment("Curse of ignorance")
-                        .category(ConfigID.CURSE_OF_IGNORANCE, CurseOfIgnorance.ENTRIES);
+                BUILDER.comment("Curse of ignorance").category(ConfigID.CURSE_OF_IGNORANCE, CurseOfIgnorance.ENTRIES);
 
         public static final class CurseOfIgnorance {
-            private  static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
+            private static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
 
             public static final ConfigEntry.BooleanEntry HIDE_NAME = BUILDER.define(ConfigID.HIDE_NAME, true);
 
@@ -133,32 +131,31 @@ public class ServerConfig {
         }
 
         public static final ConfigEntries DISTORTION =
-                BUILDER.comment("Distortion")
-                        .category(ConfigID.DISTORTION, Distortion.ENTRIES);
+                BUILDER.comment("Distortion").category(ConfigID.DISTORTION, Distortion.ENTRIES);
 
-                public static final class Distortion {
-            private  static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
+        public static final class Distortion {
+            private static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
 
-            public static final ConfigEntry.DoubleEntry ANGLE_PER_LEVEL = BUILDER.defineInRange(ConfigID.ANGLE_PER_LEVEL, 7.5, 0.0, Double.MAX_VALUE);
+            public static final ConfigEntry.DoubleEntry ANGLE_PER_LEVEL =
+                    BUILDER.defineInRange(ConfigID.ANGLE_PER_LEVEL, 7.5, 0.0, Double.MAX_VALUE);
 
             public static final ConfigEntries ENTRIES = BUILDER.build();
         }
 
         public static final ConfigEntries ALMIGHTY =
-                BUILDER.comment("Almighty")
-                        .category(ConfigID.ALMIGHTY, Almighty.ENTRIES);
+                BUILDER.comment("Almighty").category(ConfigID.ALMIGHTY, Almighty.ENTRIES);
 
         public static final class Almighty {
             private static final ConfigEntryBuilder BUILDER = new ConfigEntryBuilder();
 
-            public static final ConfigEntry.BooleanEntry EFFECTIVE_FOR_ALL_BLOCKS
-                    = BUILDER.define(ConfigID.EFFECTIVE_FOR_ALL_BLOCKS, true);
+            public static final ConfigEntry.BooleanEntry EFFECTIVE_FOR_ALL_BLOCKS =
+                    BUILDER.define(ConfigID.EFFECTIVE_FOR_ALL_BLOCKS, true);
 
             public static final ConfigEntry.BooleanEntry CAN_BREAK_UNBREAKABLE =
                     BUILDER.define(ConfigID.CAN_BREAK_UNBREAKABLE, true);
 
-            public static final ConfigEntry.DoubleEntry UNBREAKABLE_BLOCK_HARDNESS
-                    = BUILDER.defineInRange(ConfigID.UNBREAKABLE_BLOCK_HARDNESS, 100.0, 0.0, Double.MAX_VALUE);
+            public static final ConfigEntry.DoubleEntry UNBREAKABLE_BLOCK_HARDNESS =
+                    BUILDER.defineInRange(ConfigID.UNBREAKABLE_BLOCK_HARDNESS, 100.0, 0.0, Double.MAX_VALUE);
 
             public static final ConfigEntries ENTRIES = BUILDER.build();
         }
